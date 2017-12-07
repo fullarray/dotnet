a33e9723c1c20f2b3a887754bf8b12ec3f90812d
Jonathan <sifreed77@gmail.com>
2017-12-06 23:45:43 -0500

    Updated file

--- app7/BlogSettings.cs
+++ app7/BlogSettings.cs
@@ -3,7 +3,7 @@
     public class BlogSettings
     {
         public string Name { get; set; } = "app7";
-        public string Description { get; set; } = "A short description of the blog";
+        public string Description { get; set; } = "This is a blog template for general use.";
         public string Owner { get; set; } = "The Owner";
         public int PostsPerPage { get; set; } = 2;
         public int CommentsCloseAfterDays { get; set; } = 10;
