1f85d031ef5d1f846f607a125bd463335bed78e2
Jonathan <sifreed77@gmail.com>
2018-03-29 00:20:48 -0400

    Added app7 implementation

---
+++ mvc/app7/BlogSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace app7
+{
+    public class BlogSettings
+    {
+        public string Name { get; set; } = "app7";
+        public string Description { get; set; } = "A short description of the blog";
+        public string Owner { get; set; } = "The Owner";
+        public int PostsPerPage { get; set; } = 2;
+        public int CommentsCloseAfterDays { get; set; } = 10;
+    }
+}
