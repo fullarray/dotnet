7bfe40029e669c0f6e9b5d02f2734ec1db905ef7
Jonathan <sifreed77@gmail.com>
2018-06-06 22:27:58 -0400

    Added app8 implementation

---
+++ mvc/app8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
