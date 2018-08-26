7877fecb7d91221805d791e65aad592e013e81c6
Jonathan <sifreed77@gmail.com>
2018-08-25 23:10:54 -0400

    Update SampleDataController.cs

--- vue/app8/Controllers/SampleDataController.cs
+++ vue/app8/Controllers/SampleDataController.cs
@@ -11,7 +11,7 @@ namespace app8.Controllers
     {
         private static string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Incendiary"
         };
 
         [HttpGet("[action]")]
