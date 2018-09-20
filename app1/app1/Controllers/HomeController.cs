96224e325c00f269b3c7eae133ad1bd29cdc6817
Jonathan <sifreed77@gmail.com>
2018-09-19 23:34:32 -0400

    Update HomeController.cs

--- app1/app1/Controllers/HomeController.cs
+++ app1/app1/Controllers/HomeController.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-
 //This controller manages only simple aspects of the application.
-
 namespace app1.Controllers
 {
     public class HomeController : Controller
