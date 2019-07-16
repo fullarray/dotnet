51c13060081b78fead7ed1a213a2c349099c2d78
Jonathan <sifreed77@gmail.com>
2019-07-15 23:54:57 -0400

    Update TodoItem.cs

--- api/app1/Models/TodoItem.cs
+++ api/app1/Models/TodoItem.cs
@@ -1,7 +1,7 @@
 namespace TodoApi.Models{
 	public class TodoItem{
-		public long Id { get; set; }
-		public string Name { get; set; }
-		public bool IsComplete { get; set; }
+		public long Id { get; set;}
+		public string Name { get; set;}
+		public bool IsComplete { get; set;}
 	}
 }
