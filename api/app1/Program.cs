086e01ae4136de119782e9f112d1085388de0ec6
Jonathan <sifreed77@gmail.com>
2019-08-16 23:15:23 -0400

    Update Program.cs

--- api/app1/Program.cs
+++ api/app1/Program.cs
@@ -13,8 +13,7 @@ namespace app1
     public class Program
     {
         public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
+        {BuildWebHost(args).Run();
         }
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
