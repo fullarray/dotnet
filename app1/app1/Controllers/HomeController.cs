d2ee9c9123f4e0b323fe112d0e69be2a5c414915
Jonathan <sifreed77@gmail.com>
2019-09-19 23:32:59 -0400

    Update HomeController.cs

--- app1/app1/Controllers/HomeController.cs
+++ app1/app1/Controllers/HomeController.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-//This controller manages only simple aspects of the application.
+//this controller manages only simple aspects of the application.
 namespace app1.Controllers
 {
     public class HomeController : Controller
