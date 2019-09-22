3c62e75535964de9a10e14fa624e244e52c6b283
Jonathan <sifreed77@gmail.com>
2019-09-21 22:20:59 -0400

    Update Program.cs

--- angular/app10/Program.cs
+++ angular/app10/Program.cs
@@ -13,10 +13,7 @@ namespace app10
     public class Program
     {
         public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
-
+        {            BuildWebHost(args).Run();}
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
