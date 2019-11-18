6853d8447e6e49f49057996b99e7a2fd529e515f
Jonathan <sifreed77@gmail.com>
2019-11-17 22:38:15 -0500

    Update IBlogService.cs

--- app7/Services/IBlogService.cs
+++ app7/Services/IBlogService.cs
@@ -10,19 +10,12 @@ namespace app7
     public interface IBlogService
     {
         Task<IEnumerable<Post>> GetPosts(int count, int skip = 0);
-
         Task<IEnumerable<Post>> GetPostsByCategory(string category);
-
         Task<Post> GetPostBySlug(string slug);
-
         Task<Post> GetPostById(string id);
-
         Task<IEnumerable<string>> GetCategories();
-
         Task SavePost(Post post);
-
         Task DeletePost(Post post);
-
         Task<string> SaveFile(byte[] bytes, string fileName, string suffix = null);
     }
 
