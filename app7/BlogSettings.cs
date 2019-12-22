52c6fbd03103bca10b9e7a06ea26f7b1671751b8
Jonathan <sifreed77@gmail.com>
2019-12-21 23:52:07 -0500

    Update BlogSettings.cs

--- app7/BlogSettings.cs
+++ app7/BlogSettings.cs
@@ -3,7 +3,7 @@
     public class BlogSettings
     {
         public string Name { get; set; } = "app7";
-        public string Description { get; set; } = "This is a blog template for general use.";
+        public string Description { get; set; } = "This is a blogs template for general use.";
         public string Owner { get; set; } = "The Man";
         public int PostsPerPage { get; set; } = 2;
         public int CommentsCloseAfterDays { get; set; } = 10;
