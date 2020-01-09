8eb99d661c2935c4f67777ea00eb381dea14bb7b
Jonathan <sifreed77@gmail.com>
2020-01-08 23:53:38 -0500

    Update Startup.cs

--- app1/app1/Hubs/Startup.cs
+++ app1/app1/Hubs/Startup.cs
@@ -9,7 +9,7 @@ namespace app1
     {
         public void Configuration(IAppBuilder app)
         {
-            // Any connection or hub wire up and configuration should go here
+            //Any connection or hub wire up and configuration should go here
             app.MapSignalR();
         }
     }
