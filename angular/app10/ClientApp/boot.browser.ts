e73ed66ee7069430cc69492478c596c8a20588f2
Jonathan <sifreed77@gmail.com>
2020-01-18 23:52:23 -0500

    Update boot.browser.ts

--- angular/app10/ClientApp/boot.browser.ts
+++ angular/app10/ClientApp/boot.browser.ts
@@ -17,7 +17,6 @@ if (module.hot) {
 } else {
     enableProdMode();
 }
-
 // Note: @ng-tools/webpack looks for the following expression when performing production
 // builds. Don't change how this line looks, otherwise you may break tree-shaking.
 const modulePromise = platformBrowserDynamic().bootstrapModule(AppModule);
