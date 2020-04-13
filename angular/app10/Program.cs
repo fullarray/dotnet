e56c65a5c3a82fc9f1edbad9d5aa3df7e350454b
Jonathan <sifreed77@gmail.com>
2020-04-12 23:52:57 -0400

    Update Program.cs

--- angular/app10/Program.cs
+++ angular/app10/Program.cs
@@ -9,10 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace app10
-{
-    public class Program
-    {
-        public static void Main(string[] args)
+{public class Program
+    {public static void Main(string[] args)
         {            BuildWebHost(args).Run();}
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
