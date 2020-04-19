e606924de124672776c5993291ceec4de37ffa8b
Jonathan <sifreed77@gmail.com>
2020-04-18 23:37:36 -0400

    Update Program.cs

--- app2/Program.cs
+++ app2/Program.cs
@@ -1,10 +1,9 @@
 using System;
-
 namespace app2
 {    class Program
     {   static void Main(string[] args)
         {
-            Console.Write("Enter one number ");
+            Console.Write("Enter one numbers ");
             int num;
             num = Convert.ToInt32(Console.ReadLine());
             int k;
