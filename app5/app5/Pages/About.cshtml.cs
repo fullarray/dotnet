e28a06ffb0e8f7ba805f838a16dba0a5116bfd21
Jonathan <sifreed77@gmail.com>
2020-06-16 23:25:05 -0400

    Update About.cshtml.cs

--- app5/app5/Pages/About.cshtml.cs
+++ app5/app5/Pages/About.cshtml.cs
@@ -9,10 +9,9 @@ namespace app5.Pages
     public class AboutModel : PageModel
     {
         public string Message { get; set; }
-
         public void OnGet()
         {
-            Message = "Your application description page.";
+            Message = "Yours application description page.";
         }
     }
 }
