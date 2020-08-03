da39d488e6c884700054660e221b3bf71a284a0a
Jonathan <sifreed77@gmail.com>
2020-08-02 23:52:33 -0400

    Update Startup.cs

--- angular/app10/Startup.cs
+++ angular/app10/Startup.cs
@@ -19,7 +19,7 @@ namespace app10
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the containers.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
