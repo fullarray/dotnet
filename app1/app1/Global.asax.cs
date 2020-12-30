229bd36df63e27f9bf657e8b12daeb14e3c798d6
Jonathan <sifreed77@gmail.com>
2020-12-29 23:25:24 -0500

    Update Global.asax.cs

--- app1/app1/Global.asax.cs
+++ app1/app1/Global.asax.cs
@@ -8,8 +8,7 @@ using System.Web.Routing;
 
 namespace app1
 {    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
+    {protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
