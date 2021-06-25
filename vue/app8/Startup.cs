fe6ab3b96596e0051987c6665bbb6cbf19ed915d
Jonathan <sifreed77@gmail.com>
2021-06-24 20:43:32 -0400

    Update Startup.cs

--- vue/app8/Startup.cs
+++ vue/app8/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+
 using Microsoft.Extensions.Configuration;
+
 using Microsoft.Extensions.DependencyInjection;
 
 namespace app8
