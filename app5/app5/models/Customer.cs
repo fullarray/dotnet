cec9d335dc56e937f51109a1221105f24d75fef7
Jonathan <sifreed77@gmail.com>
2021-08-10 20:55:11 -0400

    Update Customer.cs

--- app5/app5/models/Customer.cs
+++ app5/app5/models/Customer.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.ComponentModel.DataAnnotations;
+
 using System.ComponentModel.DataAnnotations.Schema;
+
 using System.Threading.Tasks;
 
 namespace app5.models
